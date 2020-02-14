8c47d3da53c4809b73009438909ca2cda24f0212
Tammie Chung <tammie.chung11@gmail.com>
2020-02-14 17:43:28 -0500

    Play around with values - no significant changes

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Circle = ({x, y, onTextChange, setCircle, ...props}) => {
   return (
     <g>
-      <circle cx={x} cy={y} r="150px" fill="#529fca" />
-      <foreignObject x={x - 50} y={y - 50} width='100px' height='20px'>
+      <circle cx={x} cy={y} r="100px" fill="#529fca" />
+      <foreignObject x={x - 40} y={y - 50} width='80px' height='20px'>
         <input 
           value={x} 
           type='text'
           onChange={e => onTextChange(setCircle, 'x', e.target.value)}
         />
       </foreignObject>
-      <foreignObject x={x - 50} y={y + 30} width='100px' height='20px'>
+      <foreignObject x={x - 40} y={y + 30} width='80px' height='20px'>
         <input 
           value={y} 
           type='text' 
