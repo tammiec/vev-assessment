cda7a7e49d898c3d7c18086c70d647834aeb877b
Tammie Chung <tammie.chung11@gmail.com>
2020-02-17 19:09:12 -0500

    Group text inputs together

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -7,10 +7,27 @@ const Circle = ({x, y, onTextChange, setCircle, ...props}) => {
 
   return (
     <Draggable>
-      <g className='circle-container'>
-        <circle cx={x} cy={y} r="100px" fill="#529fca" />
+      <g>
+          <circle cx={x} cy={y} r="100px" fill="#529fca" />
+          <foreignObject x={x - 100} y={y - 100} width='200px' height='200px'>
+            <div className='circle-text'>
+              <div className='text-container'>
+                <p>x:</p>
+                <input 
+                  value={x} 
+                  type='text'
+                  onChange={e => onTextChange(setCircle, 'x', e.target.value)}
+                />
+                <p>y:</p>
+                <input 
+                  value={y} 
+                  type='text' 
+                  onChange={e => onTextChange(setCircle, 'y', e.target.value)}
+                />
+              </div>
+            </div>
+          </foreignObject>
       </g>
-
     </Draggable>
   );
 };
