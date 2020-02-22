c52d6504289d78474363334c39debbbe196be22f
Tammie Chung <tammie.chung11@gmail.com>
2020-02-22 18:20:52 -0500

    Add cursor pointer

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,10 +1,8 @@
 import React, { useRef } from 'react';
 import './Circle.css';
-const d3 = require('d3');
 
 const Circle = ({circle, onTextChange, setCircle, onMouseUp, onMouseDown, ...props}) => {
 
-  // Use useRef to create the function once and hold a reference to it.
   const handleMouseMove = useRef((e) => {
     setCircle(position => {
       const xDiff = position.coords.x - e.pageX;
@@ -22,7 +20,7 @@ const Circle = ({circle, onTextChange, setCircle, onMouseUp, onMouseDown, ...pro
 
   return (
     <>
-      <g onMouseDown={e => onMouseDown(e, setCircle, handleMouseMove)} onMouseUp={e => onMouseUp(e, setCircle, handleMouseMove)} >
+      <g className='circle' onMouseDown={e => onMouseDown(e, setCircle, handleMouseMove)} onMouseUp={e => onMouseUp(e, setCircle, handleMouseMove)} >
         <circle cx={circle.x} cy={circle.y} r="100px" fill="#529fca" />
         <foreignObject x={circle.x - 100} y={circle.y - 100} width='200px' height='200px'>
           <div className='circle-text'>
